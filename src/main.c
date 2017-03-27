de611d5fe530a0bfb33257b1ce19d76b4eddb13e
James Wright <james@jamesswright.co.uk>
2017-03-27 18:00:22 +0100

    Initial implementation. WIP input validation

---
+++ src/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const int MAX_BUFFER = 250;
+
+int main(int argc, char *argv[]) {
+    char *error_message = (char *)  malloc(MAX_BUFFER * sizeof(char *));
+    int error_code = validate_args(argc, argv, error_message);
+
+    if (error_code > 0) {
+        fputs(error_message, stderr);
+    }
+
+    return error_code;
+}
+
+int validate_args(int argc, char *argv[], char *out_message) {
+    if (strcmp(argv[1], "compile") != 0) {
+        sprintf(
+            out_message,
+            "Unrecognised command %s!\n\nUsage: numberwang compile <source file>\n",
+            argv[1]
+        );
+
+        return 1;
+    }
+
+    return 0;
+}
--- test/end_to_end.bash
+++ test/end_to_end.bash
@@ -1,21 +1,25 @@
 #!/usr/bin/env bash
+function unrecognised_command_test {
+	result=$(./bin/numberwang foo)
+	should_be_equal "$result" "Unrecognised command \"foo\"!\n\nUsage: numberwang compile <source file>"
+}
 
 function simple_test {
 	./bin/numberwang compile examples/simple.nw
 	result=$(./bin/simple)
-	should_be_equal $result "5"
+	should_be_equal "$result" "5"
 }
 
 function sun_earth_test {
 	./bin/numberwang compile examples/sun-earth-light-time.nw
 	result=$(./bin/sun-earth-light-time)
-	should_be_equal $result "8.316666666666666"
+	should_be_equal "$result" "8.316666666666666"
 }
 
 function advanced_test {
 	./bin/numberwang compile examples/advanced.nw
 	result=$(./bin/advanced)
-	should_be_equal $result "24"
+	should_be_equal "$result" "24"
 }
 
 function should_be_equal {
@@ -29,6 +33,7 @@ function should_be_equal {
 	fi
 }
 
+unrecognised_command_test
 simple_test
 sun_earth_test
 advanced_test
